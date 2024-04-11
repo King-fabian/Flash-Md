032b13310456d0150c218b240e75304a4cf493c1
King-fabian <164608464+King-fabian@users.noreply.github.com>
2024-04-11 14:40:21 +0300

    Update uptime.js

--- commandes/uptime.js
+++ commandes/uptime.js
@@ -2,6 +2,7 @@ const { france } = require("../framework/france");
 const moment = require("moment-timezone");
 const { getBuffer } = require("../framework/dl/Function");
 const { default: axios } = require('axios');
+const speed = require(performance-now);
 
 const runtime = function (seconds) { 
  seconds = Number(seconds); 

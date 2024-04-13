f23b06d3aab98f95d71ec078f48d8f1cc84bdc7a
King-fabian <164608464+King-fabian@users.noreply.github.com>
2024-04-13 23:40:02 +0300

    Update AI.js

--- commandes/AI.js
+++ commandes/AI.js
@@ -13,7 +13,7 @@ france({nomCom:"flash",reaction:"📡",categorie:"IA"},async(dest,zk,commandeOpt
   const {repondre,auteurMessage,ms,arg}=commandeOptions;
   let nom = nomAuteurMessage; 
     if(!arg || !arg[0])
-    {return repondre("yes + nom +, I'm listening to you.")}
+    {return repondre("yes" + nom + "I'm listening to you.")}
     //var quest = arg.join(' ');
   try{
     

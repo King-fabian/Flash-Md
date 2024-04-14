4632f72294b94a2cb167b6215aba4235ad22f58b
King-fabian <164608464+King-fabian@users.noreply.github.com>
2024-04-14 22:44:26 +0300

    Update test.js

--- commandes/test.js
+++ commandes/test.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { france } = require("../framework/france");
-france({ nomCom: "repo", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+france({ nomCom: "repo", alias: "script", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
 
 
 const githubRepo = 'https://api.github.com/repos/franceking1/Flash-Md';

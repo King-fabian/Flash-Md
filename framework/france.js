859e72359c2c4f787a5f9f1e43e8886318c524d5
King-fabian <164608464+King-fabian@users.noreply.github.com>
2024-04-17 02:15:10 +0300

    Update france.js

--- framework/france.js
+++ framework/france.js
@@ -1,7 +1,13 @@
 var tabCmds = [];
 let cm = [];
 function france(obj, fonctions) {
-
+    let infoComs = obj;
+    if (!obj.categorie) {
+        infoComs.categorie = "";
+    }
+    if (!obj.reaction) {
+        infoComs.reaction = "";
+    }
     infoComs.fonction = fonctions;
     cm.push(infoComs);
     // console.log('chargement...')

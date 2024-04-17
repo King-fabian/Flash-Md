1ef584c95de86c657b93478a413828a55a587d75
King-fabian <164608464+King-fabian@users.noreply.github.com>
2024-04-17 09:24:17 +0300

    Update download.js

--- commandes/download.js
+++ commandes/download.js
@@ -12,7 +12,7 @@ france({nomCom : "insta" , categorie : "Download"},async (dest , zk , commandeOp
 
   try {
      
-    let igvid = await axios('https://api.maher-zubair.tech/download/instagram?url='+link)
+    let igvid = await axios('https://api.maher-zubair.tech/download/instagram?url='+url)
 
     if (igvid.data.data.data[0].type == 'video') {
     zk.sendMessage(dest,{video : {url : igvid.data.data.data[0].url},caption : "Here is your Instagram Video.\n _Downloaded by_ *FLASH-MD*",gifPlayback : false },{quoted : ms}) 

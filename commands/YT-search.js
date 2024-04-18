b9227975bebd930ef0bdd999da7a245045a63bc8
King-fabian <164608464+King-fabian@users.noreply.github.com>
2024-04-18 11:26:05 +0300

    Update YT-search.js

--- commands/YT-search.js
+++ commands/YT-search.js
@@ -1,101 +1,50 @@
 const { france } = require("../framework/france");
-const yts = require('yt-search');
+const { getytlink, ytdwn } = require("../framework/ytdl-core");
+const yts = require("yt-search");
 const ytdl = require('ytdl-core');
 const fs = require('fs');
-const yt=require("../framework/dl/ytdl-core.js")
-const ffmpeg = require("fluent-ffmpeg");
-const yts1 = require("youtube-yts");
-//var fs =require("fs-extra")
 
-france({
-  nomCom: "play",
-  categorie: "Search",
-  reaction: "💿"
-}, async (origineMessage, zk, commandeOptions) => {
+france({ nomCom: "yts", categorie: "Search", reaction: "✋" }, async (dest, zk, commandeOptions) => {
   const { ms, repondre, arg } = commandeOptions;
-     
-  if (!arg[0]) {
-    repondre("wich song do you want.");
+  const query = arg.join(" ");
+
+  if (!query[0]) {
+    repondre("what do you want");
     return;
   }
 
   try {
-    let topo = arg.join(" ")
-    const search = await yts(topo);
-    const videos = search.videos;
+    const info = await yts(query);
+    const resultat = info.videos;
 
-    if (videos && videos.length > 0 && videos[0]) {
-      const urlElement = videos[0].url;
-          
-       let infoMess = {
-          image: {url : videos[0]. thumbnail},
-         caption : `\n*song name :* _${videos[0].title}_
-
-*Time :* _${videos[0].timestamp}_
-
-*Url :* _${videos[0].url}_
-
-
-_*DOWNLOADING...*_\n\n`
-       }
-
-      
-
-      
-
-      
-       zk.sendMessage(origineMessage,infoMess,{quoted:ms}) ;
-      // Obtenir le flux audio de la vidéo
-      const audioStream = ytdl(urlElement, { filter: 'audioonly', quality: 'highestaudio' });
-
-      // Nom du fichier local pour sauvegarder le fichier audio
-      const filename = 'audio.mp3';
-
-      // Écrire le flux audio dans un fichier local
-      const fileStream = fs.createWriteStream(filename);
-      audioStream.pipe(fileStream);
-
-      fileStream.on('finish', () => {
-        // Envoi du fichier audio en utilisant l'URL du fichier local
-      
-
-     zk.sendMessage(origineMessage, { audio: { url:"audio.mp3"},mimetype:'audio/mp4' }, { quoted: ms,ptt: false });
-        console.log("Envoi du fichier audio terminé !");
-
-     
-      });
-
-      fileStream.on('error', (error) => {
-        console.error('Erreur lors de l\'écriture du fichier audio :', error);
-        repondre('Une erreur est survenue lors de l\'écriture du fichier audio.');
-      });
-    } else {
-      repondre('Aucune vidéo trouvée.');
+    let captions = "";
+    for (let i = 0; i < 10; i++) {
+      captions += `----------------\nTitle: ${resultat[i].title}\nTime : ${resultat[i].timestamp}\nUrl: ${resultat[i].url}\n`;
     }
+    captions += "\n======\n*powered by FLASH-MD*";
+
+    // repondre(captions)
+    zk.sendMessage(dest, { image: { url: resultat[0].thumbnail }, caption: captions }, { quoted: ms });
   } catch (error) {
-    console.error('Erreur lors de la recherche ou du téléchargement de la vidéo :', error);
-    
-    repondre('Une erreur est survenue lors de la recherche ou du téléchargement de la vidéo.');
+    repondre("Erreur lors de la procédure : " + error);
   }
 });
 
-  
-
 france({
-  nomCom: "video",
-  categorie: "Search",
+  nomCom: "ytmp4",
+  categorie: "Download",
   reaction: "🎥"
 }, async (origineMessage, zk, commandeOptions) => {
   const { arg, ms, repondre } = commandeOptions;
 
   if (!arg[0]) {
-    repondre("insert video name");
+    repondre("insert a youtube link");
     return;
   }
 
   const topo = arg.join(" ");
   try {
-    const search = await yts(topo);
+    /* const search = await yts(topo);
     const videos = search.videos;
 
     if (videos && videos.length > 0 && videos[0]) {
@@ -103,40 +52,81 @@ france({
 
       let InfoMess = {
         image: { url: videos[0].thumbnail },
-        caption: `*Video name :* _${Element.title}_
-*Time :* _${Element.timestamp}_
-*Url :* _${Element.url}_
-_*DOWNLOADING...*_\n\n`
+        caption: `*nom de la vidéo :* _${Element.title}_
+*Durée :* _${Element.timestamp}_
+*Lien :* _${Element.url}_
+_*En cours de téléchargement...*_\n\n`
       };
 
       zk.sendMessage(origineMessage, InfoMess, { quoted: ms });
+    */
 
-      // Obtenir les informations de la vidéo à partir du lien YouTube
-      const videoInfo = await ytdl.getInfo(Element.url);
-      // Format vidéo avec la meilleure qualité disponible
-      const format = ytdl.chooseFormat(videoInfo.formats, { quality: '18' });
-      // Télécharger la vidéo
-      const videoStream = ytdl.downloadFromInfo(videoInfo, { format });
+    // Obtenir les informations de la vidéo à partir du lien YouTube
+    const videoInfo = await ytdl.getInfo(topo);
+    // Format vidéo avec la meilleure qualité disponible
+    const format = ytdl.chooseFormat(videoInfo.formats, { quality: '18' });
+    // Télécharger la vidéo
+    const videoStream = ytdl.downloadFromInfo(videoInfo, { format });
 
-      // Nom du fichier local pour sauvegarder la vidéo
-      const filename = 'video.mp4';
+    // Nom du fichier local pour sauvegarder la vidéo
+    const filename = 'video.mp4';
 
-      // Écrire le flux vidéo dans un fichier local
-      const fileStream = fs.createWriteStream(filename);
-      videoStream.pipe(fileStream);
+    // Écrire le flux vidéo dans un fichier local
+    const fileStream = fs.createWriteStream(filename);
+    videoStream.pipe(fileStream);
 
-      fileStream.on('finish', () => {
-        // Envoi du fichier vidéo en utilisant l'URL du fichier local
-        zk.sendMessage(origineMessage, { video: { url :"./video.mp4"} , caption: "*FLASH-MD*", gifPlayback: false }, { quoted: ms });
-      });
+    fileStream.on('finish', () => {
+      // Envoi du fichier vidéo en utilisant l'URL du fichier local
+      zk.sendMessage(origineMessage, { video: { url: `./${filename}` }, caption: "Powered by *FLASH-MD*", gifPlayback: false }, { quoted: ms });
 
-      fileStream.on('error', (error) => {
-        console.error('Erreur lors de l\'écriture du fichier vidéo :', error);
-        repondre('Une erreur est survenue lors de l\'écriture du fichier vidéo.');
-      });
-    } else {
-      repondre('No video found');
-    }
+    });
+
+    fileStream.on('error', (error) => {
+      console.error('Erreur lors de l\'écriture du fichier vidéo :', error);
+      repondre('Une erreur est survenue lors de l\'écriture du fichier vidéo.');
+    });
+
+  } catch (error) {
+    console.error('Erreur lors de la recherche ou du téléchargement de la vidéo :', error);
+    repondre('Une erreur est survenue lors de la recherche ou du téléchargement de la vidéo.' + error);
+  }
+});
+
+france({
+  nomCom: "ytmp3",
+  categorie: "Download",
+  reaction: "💿"
+}, async (origineMessage, zk, commandeOptions) => {
+  const { ms, repondre, arg } = commandeOptions;
+
+  if (!arg[0]) {
+    repondre("Insert a youtube link");
+    return;
+  }
+
+  try {
+    let topo = arg.join(" ");
+
+    const audioStream = ytdl(topo, { filter: 'audioonly', quality: 'highestaudio' });
+
+    // Nom du fichier local pour sauvegarder le fichier audio
+    const filename = 'audio.mp3';
+
+    // Écrire le flux audio dans un fichier local
+    const fileStream = fs.createWriteStream(filename);
+    audioStream.pipe(fileStream);
+
+    fileStream.on('finish', () => {
+      // Envoi du fichier audio en utilisant l'URL du fichier local
+      zk.sendMessage(origineMessage, { audio: { url: `./${filename}` }, mimetype: 'audio/mp4' }, { quoted: ms, ptt: false });
+      console.log("Envoi du fichier audio terminé !");
+    });
+
+    fileStream.on('error', (error) => {
+      console.error('Erreur lors de l\'écriture du fichier audio :', error);
+      repondre('Une erreur est survenue lors de l\'écriture du fichier audio.');
+    });
+
   } catch (error) {
     console.error('Erreur lors de la recherche ou du téléchargement de la vidéo :', error);
     repondre('Une erreur est survenue lors de la recherche ou du téléchargement de la vidéo.');

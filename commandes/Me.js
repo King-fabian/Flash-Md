7d9e9fdde23c798ffe3a37c44ce568ee9c07e8ea
King-fabian <164608464+King-fabian@users.noreply.github.com>
2024-04-18 12:47:40 +0300

    🐐 🔵

--- commandes/Me.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const { france } = require("../framework/france");
-france({ nomCom: "fabian", reaction: "🤍", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
-
- const respondre(../sprinter);
-});

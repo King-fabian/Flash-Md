3da9e2a88638a727c2637f8a61b8f87645971015
King-fabian <164608464+King-fabian@users.noreply.github.com>
2024-04-20 11:37:38 +0300

    Update Download.js

--- commands/Download.js
+++ commands/Download.js
@@ -1,3 +1,4 @@
+
 const { mediafireDl } = require("../framework/dl/Function");
 const {france} = require('../framework/france');
 const fs = require('fs');
@@ -5,16 +6,45 @@ const getFBInfo = require("@xaviabot/fb-downloader");
 const { default: axios } = require('axios');
 
 
+
+
+
+
+
+/*france({nomCom : "igdl" , categorie : "Download"},async (dest , zk , commandeOptions)=>{
+  const {ms,repondre,arg} = commandeOptions ;
+
+  let link = arg.join(' ')
+
+  if (!arg[0]) { repondre('Veillez insérer un lien video instagramme');return}; 
+
+  try {
+     
+    let igvid = await axios('https://vihangayt.me/download/instagram?url='+link)
+
+    if (igvid.data.data.data[0].type == 'video') {
+    zk.sendMessage(dest,{video : {url : igvid.data.data.data[0].url},caption : "ig video downloader powered by *FLASH-MD*",gifPlayback : false },{quoted : ms}) 
+    }
+    else {
+        zk.sendMessage(dest,{image : {url : igvid.data.data.data[0].url},caption : "ig image downloader powered by *FLASH-MD*"})
+    }
+  
+  } catch (e) {repondre("erreur survenue lors du téléchargement \n " + e)}
+  
+});*/
+
+
+
 france({nomCom : "insta" , categorie : "Download"},async (dest , zk , commandeOptions)=>{
   const {ms,repondre,arg} = commandeOptions;
 
-let goat = arg.join(' ')
+let link = arg.join(' ')
 
   if (!arg[0]) { repondre('Please insert an Instagram video link');return};
 
 try {
-    const flash = await fetch(`https://api.maher-zubair.tech/download/instagram?url=${goat}`);
-    const data = await flash.json();
+    const red = await fetch(`https://api.maher-zubair.tech/download/instagram?url=${link}`);
+    const data = await red.json();
 
     if (data && data.result && data.result.data && data.result.data.length > 0) {
         const media = data.result.data[0];
@@ -33,8 +63,74 @@ try {
 });
 
 
-france({nomCom : "mediafire" , categorie : "Download"},async (dest , zk , commandeOptions)=>{
 
+
+
+
+
+
+france({ nomCom: "twitter", categorie: "Download" }, async (dest, zk, commandeOptions) => {
+    const { ms, repondre, arg } = commandeOptions;
+    let linkz = arg.join(' ');
+
+    if (!arg[0]) {
+        repondre('Please insert an Instagram video link');
+        return;
+    }
+
+
+try {
+    
+        const blue = await fetch(`https://api.maher-zubair.tech/download/twitter?url=${linkz}`);
+        const data = await blue.json();
+
+        if (data && data.data && data.data.HD) {
+            const medi = data.data.HD;
+           
+                zk.sendMessage(dest, { video: { url: medi }, caption: "Here is your Twitter Video.\n _Downloaded by_ *FLASH-MD*", gifPlayback: false }, { quoted: ms });
+
+}
+           
+        } catch (e) { repondre("I am unable to download your media. \n " + e)}
+       
+            
+        
+    
+});
+
+
+
+
+france({nomCom : "tiktok" , categorie : "Download"},async (dest , zk , commandeOptions)=>{
+  const {ms,repondre,arg} = commandeOptions;
+
+let linkx = arg.join(' ')
+
+  if (!arg[0]) { repondre('Please insert a video link');return};
+
+
+const green = await fetch(`https://api.maher-zubair.tech/download/tiktok2?url=${linkx}`);
+    const data = await green.json();
+
+try {
+
+if (data && data.result && data.result.url && data.result.url.nowm) {
+
+const nowm = data.result.url.nowm;
+
+zk.sendMessage(dest, { video: { url: nowm }, caption: "Here is your Tiktok Video.\n _Downloaded by_ *FLASH-MD*", gifPlayback: false }, { quoted: ms });
+}
+
+
+} catch (e) {repondre("I am unable to download the file. \n " + e)}
+
+
+
+}); 
+
+
+
+france({nomCom : "mediafire" , categorie : "Download"},async (dest , zk , commandeOptions)=>{
   const {ms,repondre,arg} = commandeOptions ;
 
   let link = arg.join(' ')
@@ -47,7 +143,7 @@ france({nomCom : "mediafire" , categorie : "Download"},async (dest , zk , comman
         const fileInfo = await mediafireDl(link);
 
 if (fileInfo[0].size.split('MB')[0] >= 100) {
-            return m.reply('File big eh');
+            return m.reply('File tooooo big');
         }
 
 await zk.sendMessage(
@@ -58,7 +154,7 @@ await zk.sendMessage(
                 },
                 fileName: fileInfo[0].nama,
                 mimetype: fileInfo[0].mime,
-                caption: `FLASH-MD Downloader: ${fileInfo[0].nama}`,  
+                caption: `Downloaded by FLASH-MD: ${fileInfo[0].nama}`,  
             },
             { quoted: ms }
         );
@@ -70,9 +166,8 @@ await zk.sendMessage(
 });
 
 
-
 france({
-  nomCom: "fb",
+  nomCom: "fbdl",
   categorie: "Download",
   reaction: "📽️"
 },
@@ -94,50 +189,49 @@ async (dest, zk, commandeOptions) => {
         Lien: ${result.url}
       `;
        zk.sendMessage(dest,{image : { url : result.thumbnail}, caption : caption},{quoted : ms}) ;
-       zk.sendMessage(dest, { video: { url: result.hd  }, caption: 'facebook video Downloader.\n Powered by *FLASH-MD*' }, { quoted: ms });
+       zk.sendMessage(dest, { video: { url: result.hd  }, caption: 'facebook video downloader\n powered by *FLASH-MD*' }, { quoted: ms });
       
     })
     .catch((error) => {console.log("Error:", error)
-                      repondre('try fb2 on this link')});
+                      repondre('try fbdl2 on this link')});
 
 
    
   } catch (error) {
-    console.error('An Error occurred while your downloading video :', error);
-    repondre('An Error Occurred while Downloading your video' , error);
+    console.error('Erreur lors du téléchargement de la vidéo :', error);
+    repondre('Erreur lors du téléchargement de la vidéo.' , error);
   }
 });
 
 
-france({nomCom : "tiktok" , categorie : "Download"},async (dest , zk , commandeOptions)=>{
-  const {ms,repondre,arg} = commandeOptions;
 
-let tk = arg.join(' ')
+/*france({ nomCom: "tiktok", categorie: "Download", reaction: "🎵" }, async (dest, zk, commandeOptions) => {
+  const { arg, ms, prefixe,repondre } = commandeOptions;
+  if (!arg[0]) {
+    repondre(`how to use this command:\n ${prefixe}tiktok tiktok_video_link`);
+    return;
+  }
 
-  if (!arg[0]) { repondre('Please insert a Tik Tok link to be downloaded');return};
+  const videoUrl = arg.join(" ");
 
-try {
-    const tok = await fetch(`https://api.maher-zubair.tech/download/tiktok2?url=${tk}`);
-    const data = await tok.json();
+ let data = await axios.get('https://vihangayt.me/download/tiktok?url='+ videoUrl) ;
 
-    if (data && data.result && data.result.data && data.result.data.length > 0) {
-        const media = data.result.data[0];
-        if (media.type === 'video') {
-            zk.sendMessage(dest, { video: { url: media.url }, caption: "Here is your Tik Tok Video.\n _Downloaded by_ *FLASH-MD*", gifPlayback: false }, { quoted: ms });
-        } else {
-            zk.sendMessage(dest, { image: { url: media.url }, caption: "Here is your Tik Tok Image!\n _Downloaded by_ *FLASH-MD*" });
-        }
-    } else {
-        throw new Error("No media found in the response data");
-    }
-} catch (e) {
-    console.error("An error occurred while downloading:", e);
-}
+  let tik = data.data.data
 
-});
+      // Envoi du message avec le thumbnail de la vidéo
+      const caption = `
+Author: ${tik.author}
+Description: ${tik.desc}
+      `;
+
+         
+      zk.sendMessage(dest, { video: { url: tik.links[0].a} , caption : caption },{quoted : ms});    
+
+  
+});*/
 
 france({
-  nomCom: "fb2",
+  nomCom: "fbdl2",
   categorie: "Download",
   reaction: "📽️"
 },
@@ -168,34 +262,7 @@ async (dest, zk, commandeOptions) => {
 
    
   } catch (error) {
-    console.error('An Error occurred while downloading your video:', error);
-    repondre('An error occurred while downloading your video.' , error);
+    console.error('An error occurred while Flash-Md was downloading your media:', error);
+    repondre('An error occurred while Flash-Md was downloading your media.' , error);
   }
-}); 
-
-france({ nomCom: "twtdl", categorie: "Download" }, async (dest, zk, commandeOptions) => {
-    const { ms, repondre, arg } = commandeOptions;
-    let linkz = arg.join(' ');
-
-    if (!arg[0]) {
-        repondre('Please insert an Instagram video link');
-        return;
-    }
-
-
-try {
-    
-        const blue = await fetch(`https://api.maher-zubair.tech/download/twitter?url=${linkz}`);
-        const data = await blue.json();
-
-        if (data && data.data && data.data.HD) {
-            const medi = data.data.HD;
-           
-                zk.sendMessage(dest, { video: { url: medi }, caption: "Here is your Twitter Video.\n _Downloaded by_ *FLASH-MD*", gifPlayback: false }, { quoted: ms });
-           
-        } catch (e) { repondre("I am unable to download your media. \n " + e)}
-       
-            
-        
-    
 });

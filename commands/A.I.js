41242030d13a022709bc7df5ff91580b878f1d46
King-fabian <164608464+King-fabian@users.noreply.github.com>
2024-04-21 02:11:30 +0300

    Update A.I.js

--- commands/A.I.js
+++ commands/A.I.js
@@ -92,22 +92,16 @@ console.log(data.completion);
 
   });
 
-france({ nomCom: "bard", reaction: "📡", categorie: "IA" }, async (dest, zk, commandeOptions) => {
-    const { repondre, arg, ms } = commandeOptions;
-  
-    
-      if (!arg || arg.length === 0) {
-        return repondre(`*_Please provide some text to generate a response._*`);
-      }
-  
-      // Regrouper les arguments en une seule chaîne séparée par "-"
-      const goat = arg.join(' ');
-     const res = await fetch(`https://api.maher-zubair.tech/ai/bard?q=${goat}`);
-const data = await res.json();
-
-await repondre = data.result;
-console.log(data.completion);
-  });
-
-
-  
+france({ nomCom: "bard", reaction: "📡", categorie: "IA" }, async (dest, zk, commandeOptions) => { 
+    const { repondre, arg, ms } = commandeOptions; 
+    if (!arg || arg.length === 0) { 
+        return repondre(`*_Please provide some text to generate a response._*`); 
+    }  
+    // Regrouper les arguments en une seule chaîne séparée par "-"
+    const goat = arg.join('-'); 
+    const res = await fetch(`https://api.maher-zubair.tech/ai/bard?q=${goat}`);
+    const data = await res.json();
+    const response = data.result;
+    await repondre(response); 
+    console.log(data.completion); 
+});

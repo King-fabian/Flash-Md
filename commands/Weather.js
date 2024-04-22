ca47ab9f9e0b157ae343c2ec388808dc55bfce59
King-fabian <164608464+King-fabian@users.noreply.github.com>
2024-04-22 23:43:43 +0300

    Update Weather.js

--- commands/Weather.js
+++ commands/Weather.js
@@ -26,8 +26,8 @@ if (!question) return repondre("Give me location...");
         const windSpeed = data.wind.speed;
         const rainVolume = data.rain ? data.rain['1h'] : 0;
         const cloudiness = data.clouds.all;        
-        const sunrise = new Date(data.sys.sunrise *800);
-        const sunset = new Date(data.sys.sunset *600);
+        const sunrise = new Date(data.sys.sunrise * 800);
+        const sunset = new Date(data.sys.sunset * 600);
         
 
 

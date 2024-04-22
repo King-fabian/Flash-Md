1a19d59318a39be3424cfc1c38a22f543c2624ca
King-fabian <164608464+King-fabian@users.noreply.github.com>
2024-04-22 12:31:24 +0300

    Update General.js

--- commands/General.js
+++ commands/General.js
@@ -60,7 +60,7 @@ france({ nomCom: "developer", categorie: "General", reaction: "🐐" }, async (d
       // Ajoute d'autres développeurs ici avec leur nom et numéro
     ];
 
-    let message = "Hello 👋\n *Welcome to Flash-Md!*\n\n✦CREATOR INFO✦\n *◇NAME: France King*\n*◇AGE: 23*\n*◇LOCATION: On Earth*\n*◇BELIEVER: YES ✅*\n*◇FRIENDLY: YES ✅*\n*◇EGOISTIC: YEEES ✅*\n*◇FAV TEAM: CHELSEA*\n\n______________________________\n\n*💙KEEP USING FLASH-MD🤍*\n\n\n_The following are my contacts_:\n:";
+    let message = "Hello 👋\n *Welcome to Flash-Md!*\n\n*✦━━◆CREATOR INFO◆━━✦*\n *◇NAME: France King*\n*◇AGE: 23*\n*◇LOCATION: On Earth*\n*◇BELIEVER: YES ✅*\n*◇FRIENDLY: YES ✅*\n*◇EGOISTIC: YEEES ✅*\n*◇FAV TEAM: CHELSEA*\n\n______________________________\n\n*💙KEEP USING FLASH-MD🤍*\n\n\n_The following are my contacts_:\n:";
     for (const dev of devs) {
       message += `----------------\n• ${dev.nom} : https://wa.me/${dev.numero}\n`;
     }

52d44d6c4a70c726015a5c0f2357c8e0c26ab790
King-fabian <164608464+King-fabian@users.noreply.github.com>
2024-04-30 12:50:22 +0300

    Update Mods.js

--- commands/Mods.js
+++ commands/Mods.js
@@ -640,9 +640,9 @@ if (!superUser) {repondre('This command is only allowed to the bot owner') ; ret
 
 france({ nomCom: "^", categorie: "User" }, async (dest, zk, commandeOptions) => {
 
-  const { repondre , msgRepondu , superUser, auteurMessage } = commandeOptions;
+  const { repondre , msgRepondu , /*superUser*/, auteurMessage } = commandeOptions;
   
-    if ( superUser) { 
+   // if ( superUser) { 
   
       if(msgRepondu) {
 

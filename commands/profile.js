d79b1f6adc43b35d7fa9bf100cd996a63a6946bc
King-fabian <164608464+King-fabian@users.noreply.github.com>
2024-05-10 23:10:05 +0300

    Update profile.js

--- commands/profile.js
+++ commands/profile.js
@@ -84,3 +84,52 @@ france( {
          
             zk.sendMessage(dest,mess,{quoted : ms})
       }); 
+
+
+        france( {
+  nomCom : "getpp",
+ categorie : "Fun", 
+   },
+      async(dest,zk, commandeOptions)=> {
+
+        const {ms , arg, repondre,auteurMessage,nomAuteurMessage, msgRepondu , auteurMsgRepondu} = commandeOptions ;
+        let jid = null 
+          let nom = null ;
+
+          
+
+
+
+        if (!msgRepondu) {
+            jid = auteurMessage;
+           nom = nomAuteurMessage;
+
+           try { ppUrl = await zk.profilePictureUrl(jid , 'image') ; } catch { ppUrl = "https://static.animecorner.me/2023/08/op2.jpg"};
+          const status = await zk.fetchStatus(jid) ;
+
+           mess = {
+            image : { url : ppUrl },
+           /* caption : '*Nom :* '+ nom + '\n*Status :*\n' + status.status
+        } */
+          
+        } else {
+            jid = auteurMsgRepondu;
+            nom ="@"+auteurMsgRepondu.split("@")[0] ;
+
+            try { ppUrl = await zk.profilePictureUrl(jid , 'image') ; } catch { ppUrl = "https://static.animecorner.me/2023/08/op2.jpg"};
+          const status = await zk.fetchStatus(jid) ;
+
+             mess = {
+              image : { url : ppUrl },
+             // caption : '*Name :* '+ nom + '\n*Status :*\n' + status.status,
+              // mentions:[auteurMsgRepondu]
+          }
+            
+        } ;
+
+     
+      
+      
+         
+            zk.sendMessage(dest,mess,{quoted : ms})
+      }); 

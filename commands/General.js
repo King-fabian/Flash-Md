c1f8191ff438dcb9aba09024e91f743954165ac3
King-fabian <164608464+King-fabian@users.noreply.github.com>
2024-06-18 13:06:10 +0300

    Update

--- commands/General.js
+++ commands/General.js
@@ -1,8 +1,94 @@
-const { france } = require("../framework/france");
-const {getAllSudoNumbers,isSudoTableNotEmpty} = require("../bdd/sudo")
-const conf = require("../set");
+const { king } = require("../france/king");
+const {getAllSudoNumbers,isSudoTableNotEmpty} = require("../data/sudo")
+const conf = require("../set")
+const fs = require("fs");
 
-france({ nomCom: "owner", categorie: "General", reaction: "💞" }, async (dest, zk, commandeOptions) => {
+
+
+
+
+king({ nomCom: "helpers", categorie: "General", reaction: "🤍 " }, async (dest, zk, commandeOptions) => {
+    const { ms, mybotpic } = commandeOptions;
+
+    const devs = [
+      { nom: "Belta from Kenya", numero: "254114141192" }, 
+      { nom: "Kijana Ya Atwoli from Kenya", numero: "254768666135" },
+      { nom: "ZARK From Uganda", numero: "256762086532" },
+      { nom: "Ben Jason From Nigeria", numero: "2347039100348" },
+      { nom: "Kebby From Tanzania", numero: "255675514698" },
+      { nom: "Loner From Nigeria", numero: "2349152597171" },
+        { nom: "Lil Benjamin From Kenya", numero: "254769702239" },
+        { nom: "KingPin From Kenya", numero: "254743437665" },
+      // Ajoute d'autres développeurs ici avec leur nom et numéro
+    ];
+
+    let message = "👋 *Hello Thanks for CHOOSING FLASH-MD* \nThe following numbers are for ambassadors of *FLASH-MD*, \nLook for one in your country and ask for help! \n*KEEP USING FLASH-MD*💥 🤩:\n\n";
+    for (const dev of devs) {
+      message += `----------------\n• ${dev.nom} : https://wa.me/${dev.numero}\n`;
+    }
+  var lien = mybotpic()
+    if (lien.match(/\.(mp4|gif)$/i)) {
+    try {
+        zk.sendMessage(dest, { video: { url: lien }, caption:message }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+// Vérification pour .jpeg ou .png
+else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+    try {
+        zk.sendMessage(dest, { image: { url: lien }, caption:message }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+else {
+    repondre(lien)
+    repondre("link error");
+    
+}
+});
+
+
+king({ nomCom: "test", categorie: "General", reaction: "😡" }, async (dest, zk, commandeOptions) => {
+    const { ms , mybotpic, auteurMessage } = commandeOptions;
+
+
+const data = './commands/test.mp3';
+
+    const dooc = {
+        audio: {
+          url: data
+        },
+        mimetype: 'audio/mp4',
+        ptt: true,
+        waveform:  [100, 0, 100, 0, 100, 0, 100],
+        fileName: "what?",
+
+        contextInfo: {
+          mentionedJid: [auteurMessage],
+          externalAdReply: {
+          title: "FLASH-MD IS ACTIVE",
+          body: "Powered by France King",
+          thumbnailUrl: "https://telegra.ph/file/4143dfac775bff078cc5a.jpg",
+          sourceUrl: 'https://whatsapp.com/channel/0029VaTbb3p84Om9LRX1jg0P',
+          mediaType: 1,
+          renderLargerThumbnail: true
+          }}
+      };
+
+
+await zk.sendMessage(dest, dooc, {quoted: ms});
+
+});
+
+
+
+king({ nomCom: "owner", categorie: "General", reaction: "💥" }, async (dest, zk, commandeOptions) => {
     const { ms , mybotpic } = commandeOptions;
     
   const thsudo = await isSudoTableNotEmpty()
@@ -10,9 +96,9 @@ france({ nomCom: "owner", categorie: "General", reaction: "💞" }, async (dest,
   if (thsudo) {
      let msg = `*My Super-User*\n
      *Owner Number*\n :
-- 🌟 @${conf.NUMERO_OWNER}
+- 🌟 @${conf.OWNER_NUMBER}
 
------- *other sudos* -----\n`
+------ *OTHER SUDOS* -----\n`
      
  let sudos = await getAllSudoNumbers()
 
@@ -22,7 +108,7 @@ france({ nomCom: "owner", categorie: "General", reaction: "💞" }, async (dest,
       msg += `- 💼 @${sudonumero}\n`;
     } else {return}
 
-   }   const ownerjid = conf.NUMERO_OWNER.replace(/[^0-9]/g) + "@s.whatsapp.net";
+   }   const ownerjid = conf.OWNER_NUMBER.replace(/[^0-9]/g) + "@s.whatsapp.net";
    const mentionedJid = sudos.concat([ownerjid])
    console.log(sudos);
    console.log(mentionedJid)
@@ -40,7 +126,7 @@ france({ nomCom: "owner", categorie: "General", reaction: "💞" }, async (dest,
         'VERSION:3.0\n' +
         'FN:' + conf.OWNER_NAME + '\n' + // full name
         'ORG:undefined;\n' + // the organization of the contact
-        'TEL;type=CELL;type=VOICE;waid=' + conf.NUMERO_OWNER + ':+' + conf.NUMERO_OWNER + '\n' + // WhatsApp ID + phone number
+        'TEL;type=CELL;type=VOICE;waid=' + conf.OWNER_NUMBER + ':+' + conf.OWNER_NUMBER + '\n' + // WhatsApp ID + phone number
         'END:VCARD';
     zk.sendMessage(dest, {
         contacts: {
@@ -51,7 +137,7 @@ france({ nomCom: "owner", categorie: "General", reaction: "💞" }, async (dest,
   }
 });
 
-france({ nomCom: "developer", categorie: "General", reaction: "🐐" }, async (dest, zk, commandeOptions) => {
+king({ nomCom: "developer", categorie: "General", reaction: "🐐" }, async (dest, zk, commandeOptions) => {
     const { ms, mybotpic } = commandeOptions;
 
     const devs = [
@@ -92,7 +178,7 @@ else {
 }
 });
 
-france({ nomCom: "support", categorie: "General" }, async (dest, zk, commandeOptions) => {
+king({ nomCom: "support", categorie: "General" }, async (dest, zk, commandeOptions) => {
   const { ms, repondre, auteurMessage, } = commandeOptions; 
  
   repondre("look on pm sir ")
